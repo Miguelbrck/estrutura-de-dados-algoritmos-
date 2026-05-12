b2a6fb609a8892d2b4705c8240b1fa0f28f71845
Miguelbrck <miguelangelo@unipam.edu.br>
2026-05-12 23:24:06 +0000

    Aula JavaScript

---
+++ java/ex1.js
@@ -0,0 +1,16 @@
+var num=15;
+console.log(num);
+
+if(true){
+    var num= 20;
+    console.log(num);
+}
+
+console.log(num);
+
+let nome1 = "Miguel";
+const nome2 = "Bruno";
+
+console.log(nome1);
+console.log(nome2);
+
